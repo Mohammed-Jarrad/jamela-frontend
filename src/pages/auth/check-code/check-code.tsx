0e1f2b8a9fae6fcd7a31322c0fe68a99fa51f7a9
Mohammed-Jarrad <mohammed.w.jarrad@gmail.com>
2024-03-10 21:31:20 +0200

    add input otp on check code page

--- src/pages/auth/check-code/check-code.tsx
+++ src/pages/auth/check-code/check-code.tsx
@@ -1,10 +1,10 @@
 import Container from '@/components/my/container'
-import CustomInput from '@/components/my/custom-input'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from '@/components/ui/input-otp'
 import { useCheckCode } from '@/hooks/use-auth'
 import Transition from '@/utils/transition'
-import { FormEvent, useState } from 'react'
+import { FormEvent, Fragment, useState } from 'react'
 import { Helmet } from 'react-helmet'
 import { useParams } from 'react-router-dom'
 import { BeatLoader } from 'react-spinners'
@@ -17,27 +17,52 @@ const CheckCode = () => {
         e.preventDefault()
         checkCodeMutation.mutate({ code, token: String(token) })
     }
+
     return (
         <Container className="flex items-center justify-center">
             <Helmet>
                 <title>Check Code</title>
             </Helmet>
-            <Transition initial={{ opacity: 0, y: -70 }} animate={{ opacity: 1, y: 0 }} className="w-full md:w-auto">
+            <Transition
+                initial={{ opacity: 0, y: -70 }}
+                animate={{ opacity: 1, y: 0 }}
+                className="w-full md:w-auto"
+            >
                 <Card className="w-full md:w-[500px]">
                     <CardHeader>
-                        <CardTitle className="mb-6 text-muted-foreground md:text-2xl">Verify Code</CardTitle>
+                        <CardTitle className="text-muted-foreground md:text-2xl">
+                            Verify Code
+                        </CardTitle>
                     </CardHeader>
                     <CardContent>
                         <form className="w-full" onSubmit={handleCheckCode}>
-                            <CustomInput
-                                label="Enter the code"
-                                name="code"
-                                required
-                                placeholder="XXXX"
-                                type="text"
-                                onChange={(e) => setCode(e.target.value)}
-                            />
-                            <Button disabled={checkCodeMutation.isPending} className="mt-8 block md:w-[40%]">
+                            <div className="flex items-center justify-start">
+                                <InputOTP
+                                    value={code}
+                                    onChange={(value: string) => setCode(value)}
+                                    maxLength={4}
+                                    render={({ slots }: any) => (
+                                        <InputOTPGroup className="gap-2">
+                                            {slots.slice(0, 4).map((slot: any, index: number) => (
+                                                <Fragment key={index}>
+                                                    <InputOTPSlot
+                                                        className="rounded-md border"
+                                                        {...slot}
+                                                    />
+                                                    {index !== slots.length - 1 && (
+                                                        <InputOTPSeparator />
+                                                    )}
+                                                </Fragment>
+                                            ))}
+                                        </InputOTPGroup>
+                                    )}
+                                />
+                            </div>
+
+                            <Button
+                                disabled={checkCodeMutation.isPending}
+                                className="mt-8 block md:w-[40%]"
+                            >
                                 {checkCodeMutation.isPending ? (
                                     <BeatLoader className="text-center" color="white" />
                                 ) : (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ['class'],
-    content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
+    content: [
+        './pages/**/*.{ts,tsx}',
+        './components/**/*.{ts,tsx}',
+        './app/**/*.{ts,tsx}',
+        './src/**/*.{ts,tsx}',
+    ],
     prefix: '',
     theme: {
         container: {
@@ -77,10 +82,15 @@ module.exports = {
                     from: { height: 'var(--radix-accordion-content-height)' },
                     to: { height: '0' },
                 },
+                'caret-blink': {
+                    '0%,70%,100%': { opacity: '1' },
+                    '20%,50%': { opacity: '0' },
+                },
             },
             animation: {
                 'accordion-down': 'accordion-down 0.2s ease-out',
                 'accordion-up': 'accordion-up 0.2s ease-out',
+                'caret-blink': 'caret-blink 1.25s ease-out infinite',
             },
             height: {
                 header: 'var(--header-height)',
